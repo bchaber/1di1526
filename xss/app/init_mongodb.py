2017bcfc8dcae8d203b299c58e2d3a35f02349d5
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-12-10 06:27:06 +0100

    PyMongo 2.8 uses .remove instead of .delete

--- xss/app/init_mongodb.py
+++ xss/app/init_mongodb.py
@@ -1,12 +1,12 @@
 import pymongo
 db = pymongo.MongoClient("mongodb://root:root@db").db
-#db.user.delete({})
+db.user.remove({})
 db.user.insert({'username':'bach', 'password':'haslo'})
 db.user.insert({'username':'john', 'password':'snow'})
 db.user.insert({'username':'bob',  'password':'bob'})
-#db.session.delete({})
+db.session.remove({})
 db.session.insert({'sid':'deadbeef', 'username':'bach'})
-#db.posts.delete({})
+db.posts.remove({})
 db.posts.insert({'username':'bach','post':'Hello!','id':1})
 
 for u in db.user.find():
