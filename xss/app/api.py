e63af997b2b072e5120e340c09d6c66c479c0806
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2019-12-12 10:55:29 +0000

    Handle situation when API is operated with QueryString and cast string value of index to integer

--- xss/app/api.py
+++ xss/app/api.py
@@ -7,7 +7,14 @@ api = Blueprint('api', __name__, template_folder='templates')
 def get_post():
   body = request.get_json()
   if body is None:
-    body = request.args
+    body = request.args.copy()
+    index = body.get('index','')
+    try:
+      index = int(index)
+    except ValueError:
+      print(f"/api/post Couldn't cast the index value '{index}' to int")
+      index = -1
+    body['index'] = index
   print(f"/api/post {body}")
   password = dao.get_password(body.get('username'))
   if password != body.get('password'):
